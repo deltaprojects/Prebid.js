1702ff7396ad6f694e19e644fc93124d8ea9b9d9
Xi.Guan <xi.guan@deltaprojects.com>
2021-05-21 08:59:18 +0200

    Add video, native support for deltaprojects openrtb bid adapater

--- modules/deltaprojectsOpenRTBBidAdapter.js
+++ modules/deltaprojectsOpenRTBBidAdapter.js
@@ -1,189 +1,379 @@
 // jshint esversion: 6, es3: false, node: true
 'use strict';
 
-import {
-  registerBidder
-} from '../src/adapters/bidderFactory.js';
-import {
-  BANNER
-} from '../src/mediaTypes.js';
+import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
 import * as utils from '../src/utils.js';
-import { config } from '../src/config.js';
+import find from 'core-js-pure/features/array/find';
 
-const BIDDER_CODE = 'deltaprojectsOpenRTB';
+export const BIDDER_CODE = 'deltaprojectsOpenRTB';
+export const SUPPORTED_NATIVE_VER = '1.2';
+export const BIDDER_ENDPOINT_URL = 'http://testdomain:3001/test';
+export const USERSYNC_URL = 'http://testdomain:3001/cookie';
 
-const NATIVE_PARAMS = {
-  title: {
-    id: 0,
-    name: 'title'
-  },
-  icon: {
-    id: 2,
-    type: 1,
-    name: 'img'
-  },
-  image: {
-    id: 3,
-    type: 3,
-    name: 'img'
-  },
-  sponsoredBy: {
-    id: 5,
-    name: 'data',
-    type: 1
-  },
-  body: {
-    id: 4,
-    name: 'data',
-    type: 2
-  },
-  cta: {
-    id: 1,
-    type: 12,
-    name: 'data'
-  }
+/** -- isBidRequestValid --**/
+function isBidRequestValid(bid) {
+  if (!bid) return false;
+
+  if (bid.bidder !== BIDDER_CODE) return false;
+
+  return true;
 }
 
-export const spec = {
-  code: BIDDER_CODE,
-  supportedMediaTypes: [BANNER],
-  isBidRequestValid: bid => !!bid.params.mid,
-  getUserSyncs: userSync,
-  buildRequests: buildRequest,
-  interpretResponse: buildResponse
-};
+/** -- Build requests --**/
+function buildRequests(validBidRequests, bidderRequest) {
+  /** == shared ==**/
+  // -- build id
+  const id = bidderRequest.auctionId;
 
-registerBidder(spec);
+  // -- build site
+  const loc = utils.parseUrl(bidderRequest.refererInfo.referer);
+  const publisher = setOnAny(validBidRequests, 'params.publisher');
+  const siteId = setOnAny(validBidRequests, 'params.siteId');
+  const site = {
+    id: siteId,
+    domain: loc.hostname,
+    page: loc.href,
+    ref: loc.href,
+    publisher,
+  };
 
-function buildResponse(serverResponse, { bids }) {
+  // -- build device
+  const ua = navigator.userAgent;
+  const device = {
+    ua,
+    w: screen.width,
+    h: screen.height,
+  }
+
+  // -- build user, reg
+  let user = { ext: {} };
+  const regs = { ext: {} };
+  const gdprConsent = bidderRequest && bidderRequest.gdprConsent;
+  if (gdprConsent) {
+    user.ext = { consent: gdprConsent.consentString };
+    if (typeof gdprConsent.gdprApplies == 'boolean') {
+      regs.ext.gdpr = gdprConsent.gdprApplies ? 1 : 0
+    }
+  }
+
+  // -- build tmax
+  let tmax = (bidderRequest && bidderRequest.timeout > 0) ? bidderRequest.timeout : undefined;
+
+  // build bid specific
+  return validBidRequests.map(validBidRequest => {
+    const openRTBRequest = buildOpenRTBRequest(validBidRequest, id, site, device, user, tmax, regs);
+    return {
+      method: 'POST',
+      url: BIDDER_ENDPOINT_URL,
+      data: openRTBRequest,
+      options: { contentType: 'application/json' },
+      bids: [validBidRequest],
+    };
+  });
+}
+
+function buildOpenRTBRequest(validBidRequest, id, site, device, user, tmax, regs) {
+  // build impression
+  const impression = buildImpression(validBidRequest);
+
+  // build test
+  const test = utils.deepAccess(validBidRequest, 'params.test') ? 1 : 0
+
+  // build cur
+  const currency = utils.deepAccess(validBidRequest, 'params.currency');
+  const cur = currency && [currency];
+
+  // build source
+  const source = {
+    tid: validBidRequest.transactionId,
+    fd: 1,
+  }
+
+  return {
+    id,
+    imp: [impression],
+    site,
+    device,
+    user,
+    test,
+    tmax,
+    cur,
+    source,
+    regs,
+    ext: {},
+  };
+}
+
+function buildImpression(bid) {
+  const impression = {
+    id: bid.bidId,
+    tagid: bid.params.tagId,
+    ext: {},
+  };
+
+  const bannerMediaType = utils.deepAccess(bid, `mediaTypes.${BANNER}`);
+  const videoMediaType = utils.deepAccess(bid, `mediaTypes.${VIDEO}`);
+  const nativeMediaType = utils.deepAccess(bid, `mediaTypes.${NATIVE}`);
+  const isDefault = !(nativeMediaType || videoMediaType)
+
+  // banner / default
+  if (bannerMediaType || isDefault) {
+    impression.banner = buildImpressionBanner(bid, bannerMediaType);
+  }
+
+  // video
+  if (videoMediaType && videoMediaType.context === 'instream') {
+    impression.video = buildImpressionVideo(bid, videoMediaType);
+  }
+
+  // native
+  if (nativeMediaType) {
+    impression.native = buildImpressionNative(nativeMediaType);
+  }
+
+  // bidfloor
+  if (bid.params.floor) {
+    impression.bidfloor = bid.params.floor;
+  }
+
+  // ext
+  if (bid.params.bidderParams) {
+    utils._each(bid.params.bidderParams, (params, partner) => {
+      impression.ext[partner] = params;
+    });
+  }
+  return impression;
+}
+
+function buildImpressionBanner(bid, bannerMediaType) {
+  const bannerSizes = (bannerMediaType && bannerMediaType.sizes) || bid.sizes;
+  return {
+    format: utils._map(bannerSizes, ([width, height]) => ({ w: width, h: height })),
+  };
+}
+
+function buildImpressionVideo(bid, videoMediaType) {
+  const video = { placement: 1 };
+
+  // mimes
+  video.mimes = videoMediaType.mimes || [];
+
+  // minduratiion
+  video.minduration = utils.deepAccess(bid, 'params.video.minduration');
+
+  // maxduration
+  video.maxduration = utils.deepAccess(bid, 'params.video.maxduration');
+
+  // protoccol
+  if (videoMediaType.protocols) video.protocols = videoMediaType.protocols;
+
+  // w, h
+  if (videoMediaType.playerSize && videoMediaType.playerSize.length) {
+    const size = videoMediaType.playerSize[0];
+    video.w = size[0];
+    video.h = size[1];
+  }
+
+  // startdelay
+  video.startdelay = utils.deepAccess(bid, 'params.video.startdelay');
+
+  // api
+  if (videoMediaType.api) video.api = videoMediaType.api;
+
+  // playbackend
+  if (videoMediaType.playbackmethod) video.playbackmethod = videoMediaType.playbackmethod;
+
+  return video;
+}
+
+function buildImpressionNative(nativeMediaType) {
+  const assets = [];
+
+  // title
+  const title = nativeMediaType.title;
+  if (title) {
+    assets.push(setAssetRequired(title, {
+      title: { len: title.len },
+    }));
+  }
+
+  // main
+  const img = nativeMediaType.image;
+  if (img) {
+    assets.push(setAssetRequired(img, {
+      img: {
+        type: 3, // Main
+        wmin: 1,
+        hmin: 1,
+      },
+    }));
+  }
+
+  // icon
+  const icon = nativeMediaType.icon;
+  if (icon) {
+    assets.push(setAssetRequired(icon, {
+      img: {
+        type: 1, // Icon
+        wmin: 1,
+        hmin: 1,
+      },
+    }));
+  }
+
+  // body
+  const body = nativeMediaType.body;
+  if (body) {
+    assets.push(setAssetRequired(body, { data: { type: 2 } }));
+  }
+
+  // cta
+  const cta = nativeMediaType.cta;
+  if (cta) {
+    assets.push(setAssetRequired(cta, { data: { type: 12 } }));
+  }
+
+  // sponsoredby
+  const sponsoredBy = nativeMediaType.sponsoredBy;
+  if (sponsoredBy) {
+    assets.push(setAssetRequired(sponsoredBy, { data: { type: 1 } }));
+  }
+
+  utils._each(assets, (asset, id) => asset.id = id);
+  return {
+    request: JSON.stringify({
+      ver: SUPPORTED_NATIVE_VER,
+      assets,
+    }),
+    ver: SUPPORTED_NATIVE_VER,
+  };
+}
+
+/** -- Interpret response --**/
+function interpretResponse(serverResponse, { data: rtbRequest }) {
   if (!serverResponse.body) {
+    utils.logWarn('Response body is invalid, return !!');
     return;
   }
-  const { seatbid, cur } = serverResponse.body;
 
-  const bidResponses = flatten(seatbid.map(seat => seat.bid)).reduce((result, bid) => {
-    result[bid.impid - 1] = bid;
-    return result;
-  }, []);
+  const { body: { id, seatbid, cur } } = serverResponse;
+  if (!id || !seatbid) {
+    utils.logWarn('Id / seatbid of response is invalid, return !!');
+    return;
+  }
 
-  return bids.map((bid, id) => {
-    const bidResponse = bidResponses[id];
-    if (bidResponse) {
-      const res = {
-        ad: bidResponse.adm,
-        requestId: bid.bidId,
-        cpm: bidResponse.price,
-        creativeId: bidResponse.crid,
-        ttl: 360,
-        width: bidResponse.w,
-        height: bidResponse.h,
-        netRevenue: bid.netRevenue === 'net',
+  const rtbReqImps = rtbRequest.imp;
+  const bidResponses = [];
+
+  utils._each(seatbid, seatbid => {
+    utils._each(seatbid.bid, bid => {
+      const rtbImp = rtbReqImps.length === 1 ? rtbReqImps[0] : find(rtbReqImps, imp => imp.id === bid.impid);
+      const bidObj = {
+        requestId: bid.impid,
+        cpm: parseFloat(bid.price),
+        width: parseInt(bid.w),
+        height: parseInt(bid.h),
+        creativeId: bid.crid || bid.id,
+        dealId: bid.dealid || null,
         currency: cur,
-        mediaType: BANNER,
-        bidderCode: BIDDER_CODE
+        netRevenue: true,
+        ttl: 60,
       };
-      return res;
-    }
-  }).filter(Boolean);
+
+      if (rtbImp.video) {
+        bidObj.mediaType = VIDEO;
+        bidObj.vastXml = bid.adm;
+      } else if (rtbImp.native) {
+        const native = parseNativeResponse(bid.adm);
+        if (native) {
+          bidObj.mediaType = NATIVE;
+          bidObj.native = buildNativeResponse(native);
+        } else {
+          utils.logError('Invalid native in response', bid.adm);
+        }
+      } else {
+        bidObj.mediaType = BANNER;
+        bidObj.ad = bid.adm;
+        if (bid.nurl) {
+          bidObj.ad += utils.createTrackPixelHtml(decodeURIComponent(bid.nurl));
+        }
+      }
+      if (bid.ext) {
+        bidObj[BIDDER_CODE] = bid.ext;
+      }
+      bidResponses.push(bidObj);
+    });
+  });
+  return bidResponses;
 }
 
-function userSync(syncOptions, serverResponses, gdprConsent, uspConsent) {
+function buildNativeResponse(response) {
+  const native = {};
+  if (response.link) {
+    native.clickUrl = response.link.url;
+  }
+  utils._each(response.assets, asset => {
+    switch (asset.id) {
+      case 1:
+        native.title = asset.title.text;
+        break;
+      case 2:
+        native.image = buildNativeImg(asset.img);
+        break;
+      case 3:
+        native.icon = buildNativeImg(asset.img);
+        break;
+      case 4:
+        native.body = asset.data.value;
+        break;
+      case 5:
+        native.cta = asset.data.value;
+        break;
+      case 6:
+        native.sponsoredBy = asset.data.value;
+        break;
+    }
+  });
+  return native;
+}
+
+function buildNativeImg(img) {
+  if (img.w || img.h) {
+    return {
+      url: img.url,
+      width: img.w,
+      height: img.h,
+    };
+  } else {
+    return img.url;
+  }
+}
+
+/** -- Get user syncs --**/
+function getUserSyncs(syncOptions, serverResponses, gdprConsent, uspConsent) {
   const syncs = [];
   syncs.push({
     type: 'image',
-    url: '//testdomain:3001/cookie'
+    url: USERSYNC_URL,
   });
   return syncs;
 }
 
-function buildRequest(validBidRequests, bidderRequest) {
-  const page = bidderRequest.refererInfo.referer;
-  const adxDomain = 'testdomain:3001';
-  const ua = navigator.userAgent;
-  const pt = setOnAny(validBidRequests, 'params.pt') || setOnAny(validBidRequests, 'params.priceType') || 'net';
-  const tid = validBidRequests[0].transactionId;
-  const test = setOnAny(validBidRequests, 'params.test');
-  const publisher = setOnAny(validBidRequests, 'params.publisher');
-  const siteId = setOnAny(validBidRequests, 'params.siteId');
-  const currency = config.getConfig('currency.adServerCurrency');
-  const cur = currency && [currency];
-
-  const imp = validBidRequests.map((bid, id) => {
-    bid.netRevenue = pt;
-    const assets = utils._map(bid.nativeParams, (bidParams, key) => {
-      const props = NATIVE_PARAMS[key];
-      const asset = {
-        required: bidParams.required & 1,
-      };
-      if (props) {
-        asset.id = props.id;
-        let wmin, hmin, w, h;
-        let aRatios = bidParams.aspect_ratios;
-
-        if (aRatios && aRatios[0]) {
-          aRatios = aRatios[0];
-          wmin = aRatios.min_width || 0;
-          hmin = aRatios.ratio_height * wmin / aRatios.ratio_width | 0;
-        }
-
-        if (bidParams.sizes) {
-          const sizes = flatten(bidParams.sizes);
-          w = sizes[0];
-          h = sizes[1];
-        }
-
-        asset[props.name] = {
-          len: bidParams.len,
-          type: props.type,
-          wmin,
-          hmin,
-          w,
-          h
-        };
-
-        return asset;
-      }
-    }).filter(Boolean);
-
-    return {
-      id: id + 1,
-      tagid: bid.params.mid,
-      native: {
-        request: {
-          assets
-        }
-      }
-    };
-  });
-
-  const request = {
-    id: bidderRequest.auctionId,
-    site: { id: siteId, page, publisher },
-    device: { ua },
-    source: { tid, fd: 1 },
-    ext: { pt },
-    cur,
-    imp
-  };
-
-  if (test) {
-    request.is_debug = !!test;
-    request.test = 1;
+/** -- Helper methods --**/
+function parseNativeResponse(adm) {
+  let native = null;
+  try {
+    native = JSON.parse(adm);
+  } catch (e) {
+    utils.logError('Sortable bid adapter unable to parse native bid response:\n\n' + e);
   }
-  if (utils.deepAccess(bidderRequest, 'gdprConsent.gdprApplies')) {
-    request.user = { ext: { consent: bidderRequest.gdprConsent.consentString } };
-    request.regs = { ext: { gdpr: bidderRequest.gdprConsent.gdprApplies & 1 } };
-  }
+  return native;
+}
 
-  return {
-    method: 'POST',
-    url: '//' + adxDomain + '/test',
-    data: JSON.stringify(request),
-    options: {
-      contentType: 'application/json'
-    },
-    bids: validBidRequests
-  };
+function setAssetRequired(native, asset) {
+  if (native.required) asset.required = 1;
+  return asset;
 }
 
 function setOnAny(collection, key) {
@@ -195,6 +385,14 @@ function setOnAny(collection, key) {
   }
 }
 
-function flatten(arr) {
-  return [].concat(...arr);
-}
+/** -- Register -- */
+export const spec = {
+  code: BIDDER_CODE,
+  supportedMediaTypes: [BANNER, VIDEO, NATIVE],
+  isBidRequestValid,
+  buildRequests,
+  interpretResponse,
+  getUserSyncs,
+};
+
+registerBidder(spec);
